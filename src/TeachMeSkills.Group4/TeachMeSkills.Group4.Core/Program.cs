83a4f0657418ff36fb4240cee08b18f43378259a
SergeyBernat <vinex99@gmail.com>
2020-11-13 17:54:03 +0300

    feat1: add Purchaser class feat2: add Cashbox classes

--- src/TeachMeSkills.Group4/TeachMeSkills.Group4.Core/Program.cs
+++ src/TeachMeSkills.Group4/TeachMeSkills.Group4.Core/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using TeachMeSkills.Group4.Core;
 
-namespace TeachMeSkills.Group4.Core
+namespace TeachMeSkills.Group4.UI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateCashBox();
+        }
+        public static void CreateCashBox()
+        {
+            var t = new Thread(CashBox1.Run);
+            t.Start();
+            var t2 = new Thread(CashBox2.Run);
+            t2.Start();
+            var t3 = new Thread(CashBox3.Run);
+            t3.Start();
+            var t4 = new Thread(CashBox4.Run);
+            t4.Start();
         }
     }
+
 }
---
+++ src/TeachMeSkills.Group4/TeachMeSkills.Group4.UI/CashBox1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace TeachMeSkills.Group4.Core
+{
+    public class CashBox1
+    {
+        static object locker = new object();
+        public static SemaphoreSlim Semaphore { get; set; } = new SemaphoreSlim(5, 5);
+        public int cashboxNumber { get; set; } = 1;
+        public List<Purchaser> purchasers { get; set; } = GetPurchaserRND();
+        public static List<Purchaser> GetPurchaserRND()
+        {
+            var purchaserList = new List<Purchaser>();
+            for (int i = 1; i < 160; i++)
+            {
+                var purch = new Purchaser()
+                {
+                    purchaserThread = new Thread(Queue),
+                };
+                purchaserList.Add(purch);
+            }
+            return purchaserList;
+        }
+        public static void Run()
+        {
+            string writePath = @"C:\Users\vinex\source\repos\TMS-DotNet-Group4-Bernat\Cashbox_1.txt";
+            string text = "C̲h̲e̲c̲k̲ C̲a̲s̲h̲b̲o̲x̲ 1:\n";
+            using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(text);
+            }
+            CashBox1 cashBox = new CashBox1 { };
+            PurchasersThreadStart(cashBox);
+        }
+        public static void PurchasersThreadStart(CashBox1 cashBox)
+        {
+            while (cashBox.purchasers.Count != 0)
+            {
+                var purchaser = cashBox.purchasers.First();
+                purchaser.purchaserThread.Name = "Thread";
+                purchaser.purchaserThread.Start(purchaser);
+                cashBox.purchasers.Remove(purchaser);
+            }
+        }
+        public static void Queue(object purchaserBacketByThread)
+        {
+            if (Thread.CurrentThread.Name == "Thread")
+            {
+                Semaphore.Wait();
+                if (Semaphore.CurrentCount != 0)
+                {
+                    lock (locker)
+                    {
+                        Console.WriteLine("зашел");
+                        OutputToTxt(purchaserBacketByThread);
+                    }
+                    Semaphore.Release();
+                    Thread.CurrentThread.Interrupt();
+                }
+                else
+                {
+                    Console.WriteLine("уходит");
+                    Semaphore.Release();
+                }
+            }
+        }
+        public static void OutputToTxt(object purchaserBacketByThread)
+        {
+            string writePath = @"C:\Users\vinex\source\repos\TMS-DotNet-Group4-Bernat\Cashbox_1.txt";
+            List<Purchaser> currentBacketBPurchaser = new List<Purchaser>();
+            var current = (Purchaser)purchaserBacketByThread;
+            currentBacketBPurchaser.Add(current);
+            foreach (var item in currentBacketBPurchaser)
+            {
+                string buyerName;
+                buyerName = item.Name;
+                var a = item.basket;
+                List<decimal> Sum = new List<decimal>();
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine($"Check _{buyerName}_");
+                }
+                foreach (var value in a)
+                {
+                    string textName;
+                    string textPrice;
+                    textName = value.Name;
+                    textPrice = value.Price.ToString();
+                    Sum.Add(value.Price);
+                    using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                    {
+                        sw.WriteLine($"{textName} - {textPrice}$");
+                    }
+                }
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine($"G̲e̲n̲e̲r̲a̲l̲ s̲u̲m̲: {Sum.Sum()}$");
+                }
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("");
+                }
+            }
+        }
+    }
+}
